877ff2ab71b792636de04a0dd9122d8efbdb344c
Pedrobarberini <barberinipedro@gmail.com>
2025-06-17 19:16:45 -0300

    fix the javascript logic

--- CriarConta/script.js
+++ CriarConta/script.js
@@ -1,4 +1,4 @@
 document.getElementById('submit-btn').addEventListener('click', function() {
     // Redireciona para outra página
-    window.location.href = "login/index.html";
+    window.location.href = "../Dashboard/index.html";
 });
